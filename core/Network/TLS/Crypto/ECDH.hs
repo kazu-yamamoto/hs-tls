36e7af0f45910bbfb376cb2ef87cff8ea8dcb1d7
Kazu Yamamoto <kazu@iij.ad.jp>
2016-12-13 16:30:15 +0900

    adding X448.

--- core/Network/TLS/Crypto/ECDH.hs
+++ core/Network/TLS/Crypto/ECDH.hs
@@ -25,12 +25,14 @@ data ECDHPrivate = ECDHPri_P256 (Scalar Curve_P256R1)
                  | ECDHPri_P384 (Scalar Curve_P384R1)
                  | ECDHPri_P521 (Scalar Curve_P521R1)
                  | ECDHPri_X255 (Scalar Curve_X25519)
+                 | ECDHPri_X448 (Scalar Curve_X448)
                  deriving (Eq, Show)
 
 data ECDHPublic = ECDHPub_P256 (Point Curve_P256R1)
                 | ECDHPub_P384 (Point Curve_P384R1)
                 | ECDHPub_P521 (Point Curve_P521R1)
                 | ECDHPub_X255 (Point Curve_X25519)
+                | ECDHPub_X448 (Point Curve_X448)
                 deriving (Eq, Show)
 
 type ECDHKey = SharedSecret
@@ -47,6 +49,9 @@ p521 = Proxy
 x25519 :: Proxy Curve_X25519
 x25519 = Proxy
 
+x448 :: Proxy Curve_X448
+x448 = Proxy
+
 ecdhGenerateKeyPair :: MonadRandom r => Group -> r (ECDHPrivate, ECDHPublic)
 ecdhGenerateKeyPair P256   =
     (ECDHPri_P256,ECDHPub_P256) `fs` curveGenerateKeyPair p256
@@ -56,6 +61,8 @@ ecdhGenerateKeyPair P521   =
     (ECDHPri_P521,ECDHPub_P521) `fs` curveGenerateKeyPair p521
 ecdhGenerateKeyPair X25519 =
     (ECDHPri_X255,ECDHPub_X255) `fs` curveGenerateKeyPair x25519
+ecdhGenerateKeyPair X448 =
+    (ECDHPri_X448,ECDHPub_X448) `fs` curveGenerateKeyPair x448
 ecdhGenerateKeyPair _ = error "ecdhGenerateKeyPair"
 
 fs :: MonadRandom r
@@ -77,12 +84,15 @@ ecdhGetPubShared (ECDHPub_P521 pub) =
     first ECDHPub_P521 <$> deriveEncrypt p521 pub
 ecdhGetPubShared (ECDHPub_X255 pub) =
     first ECDHPub_X255 <$> deriveEncrypt x25519 pub
+ecdhGetPubShared (ECDHPub_X448 pub) =
+    first ECDHPub_X448 <$> deriveEncrypt x448 pub
 
 ecdhGetShared ::  ECDHPublic -> ECDHPrivate -> ECDHKey
 ecdhGetShared (ECDHPub_P256 pub) (ECDHPri_P256 pri) = deriveDecrypt p256 pub pri
 ecdhGetShared (ECDHPub_P384 pub) (ECDHPri_P384 pri) = deriveDecrypt p384 pub pri
 ecdhGetShared (ECDHPub_P521 pub) (ECDHPri_P521 pri) = deriveDecrypt p521 pub pri
 ecdhGetShared (ECDHPub_X255 pub) (ECDHPri_X255 pri) = deriveDecrypt x25519 pub pri
+ecdhGetShared (ECDHPub_X448 pub) (ECDHPri_X448 pri) = deriveDecrypt x448 pub pri
 ecdhGetShared _ _ = error "ecdhGetShared"
 
 encodeECDHPublic :: ECDHPublic -> (Group, Bytes)
@@ -90,16 +100,19 @@ encodeECDHPublic (ECDHPub_P256 p) = (P256, encodePoint p256 p)
 encodeECDHPublic (ECDHPub_P384 p) = (P384, encodePoint p384 p)
 encodeECDHPublic (ECDHPub_P521 p) = (P521, encodePoint p521 p)
 encodeECDHPublic (ECDHPub_X255 p) = (X25519, encodePoint x25519 p)
+encodeECDHPublic (ECDHPub_X448 p) = (X448, encodePoint x448 p)
 
 decodeECDHPublic :: Group -> Bytes -> Either CryptoError ECDHPublic
 decodeECDHPublic P256   bs = eitherCryptoError $ ECDHPub_P256 <$> decodePoint p256 bs
 decodeECDHPublic P384   bs = eitherCryptoError $ ECDHPub_P384 <$> decodePoint p384 bs
 decodeECDHPublic P521   bs = eitherCryptoError $ ECDHPub_P521 <$> decodePoint p521 bs
 decodeECDHPublic X25519 bs = eitherCryptoError $ ECDHPub_X255 <$> decodePoint x25519 bs
-decodeECDHPublic _      _  = error "decodeECDHPublic"
+decodeECDHPublic X448 bs = eitherCryptoError $ ECDHPub_X448 <$> decodePoint x448 bs
+decodeECDHPublic _ _ = error "decodeECDHPublic"
 
 ecdhPrivateGroup :: ECDHPrivate -> Group
 ecdhPrivateGroup (ECDHPri_P256 _) = P256
 ecdhPrivateGroup (ECDHPri_P384 _) = P384
 ecdhPrivateGroup (ECDHPri_P521 _) = P521
 ecdhPrivateGroup (ECDHPri_X255 _) = X25519
+ecdhPrivateGroup (ECDHPri_X448 _) = X448
