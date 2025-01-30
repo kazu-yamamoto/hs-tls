3a6b01bf1dcf2de13145639f5af2c467e13c9f25
Kazu Yamamoto <kazu@iij.ad.jp>
2025-01-30 20:52:11 +0900

    implementing KDF

---
+++ ech/Network/TLS/ECH/KDF.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Network.TLS.ECH.KDF where
+
+import Crypto.Hash.Algorithms (SHA256, SHA384, SHA512)
+import Crypto.KDF.HKDF (PRK)
+import qualified Crypto.KDF.HKDF as HKDF
+import Crypto.Number.Serialize (i2ospOf_)
+import Data.ByteString (ByteString)
+
+import Network.TLS.ECH.HPKE
+
+type Salt = ByteString
+type IKM = ByteString -- Input Keying Material
+type Info = ByteString
+type Key = ByteString
+
+class KDF a where
+    extract :: Salt -> IKM -> PRK a
+    expand :: PRK a -> Info -> Int -> Key
+
+instance KDF SHA256 where
+    extract = HKDF.extract
+    expand = HKDF.expand
+
+instance KDF SHA384 where
+    extract = HKDF.extract
+    expand = HKDF.expand
+
+instance KDF SHA512 where
+    extract = HKDF.extract
+    expand = HKDF.expand
+
+suite_id :: KEM_ID -> ByteString
+suite_id kem_id = "KEM" <> i
+  where
+    i = i2ospOf_ (fromIntegral (fromKEM_ID kem_id)) 2
+
+labeledExtract :: KDF a => KEM_ID -> Salt -> ByteString -> IKM -> PRK a
+labeledExtract kem_id salt label ikm = extract salt labeled_ikm
+  where
+    labeled_ikm = "HPKE-v1" <> suite_id kem_id <> label <> ikm
+
+labeledExpand
+    :: KDF a => KEM_ID -> PRK a -> ByteString -> ByteString -> Int -> Key
+labeledExpand kem_id prk label info len = expand prk labeled_info len
+  where
+    labeled_info =
+        i2ospOf_ len 2 <> "HPKE-v1" <> suite_id kem_id <> label <> info
